4b9983b02e579298b26138fb4235072195b96eda
Riley Hansen <hansenriley9@gmail.com>
2020-03-10 09:54:37 -0600

    Images are working

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,21 +1,18 @@
 import { films } from '../data/films.js'
 import { people} from '../data/people.js'
 
-let greetingDiv = document.querySelector('.greeting')
+let gallery = document.querySelector('.gallery')
 
 
 const maleCharacters = people.filter(person => person.gender === "male")
-console.log(maleCharacters)
 
 const femaleCharacters = people.filter(person => person.gender === "female")
-console.log(femaleCharacters)
 
-const otherCharacters = people.filter(
+const otherCharacters = people.filter(person =>
     person.gender === "n/a" || 
     person.gender === "none" || 
-    person.gender === "hermaphrodite",
+    person.gender === "hermaphrodite"
     )
-console.log(otherCharacters)
 
 let maleButton = document.querySelector('#maleButton')
 
@@ -23,18 +20,45 @@ let femaleButton = document.querySelector('#femaleButton')
 
 let otherButton = document.querySelector('#otherButton')
 
-maleButton.addEventListener('click' , function (event) { populateDOM(maleCharacters)
+maleButton.addEventListener('click' , function (event) {
+     populateDOM(maleCharacters)
 })
 
-let counter = 1
+femaleButton.addEventListener('click' , function (event) { 
+    populateDOM(femaleCharacters)
+})
+
+otherButton.addEventListener('click' , function (event) { 
+    populateDOM(otherCharacters)
+})
+
+//https:swapi.co/api/people/1/
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+
+    return url.slice(start, end)
+}
+
+function removeChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
 
 function populateDOM(characters) {
+    removeChildren(gallery)
     characters.forEach(person => {
-        console.log('$(person.url)')
+        let imageNum = getLastNumber(person.url)
         let personAnchor = document.createElement('a')
         personAnchor.href = '#'
         let personImg = document.createElement('img')
-        personImg.src = 'https:starwars-visualguide.com/assets/img/characters/${counter}.jpg'
+        personImg.src = `https:starwars-visualguide.com/assets/img/characters/${imageNum}.jpg`
 
         personImg.addEventListener('error' , event => {
             personImg.hidden = true
@@ -46,9 +70,11 @@ function populateDOM(characters) {
     })
 
     personAnchor.appendChild(personImg)
-    greetingDiv.appendChild(personAnchor)
-    counter++
+    gallery.appendChild(personAnchor)
+
 })
 
 }
+
+populateDOM(people)
 
