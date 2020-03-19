94f5ac39d65b554e59af1e89e1136036bf43e217
Riley Hansen <hansenriley9@gmail.com>
2020-03-19 16:15:51 -0600

    no repeats

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-export
+export function removeChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    
+    }
+}
+
+export function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
